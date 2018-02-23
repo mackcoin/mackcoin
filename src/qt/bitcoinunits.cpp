d382cdeb893a2c1f23a7514c250740585e311358
mack <foto@myphotoart.at>
2018-02-23 20:59:31 +0100

    changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("GEO");
-    case mBTC: return QString("mGEO");
-    case uBTC: return QString::fromUtf8("μGEO");
+    case BTC: return QString("MACK");
+    case mBTC: return QString("mMACK");
+    case uBTC: return QString::fromUtf8("μMACK");
     default: return QString("???");
     }
 }
